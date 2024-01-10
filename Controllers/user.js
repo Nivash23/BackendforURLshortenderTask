7314ef08eb82f3c3971fd32b1196ce4257044f6f
Nivash23 <nivashsubash32@gmail.com>
2024-01-10 21:44:00 +0530

    PasswordResettask

--- Controllers/user.js
+++ Controllers/user.js
@@ -3,19 +3,18 @@ const bcrypt = require('bcrypt');
 const User = require('../Model/users');
 
 userRouter.post('/', async (req, res) => {
-    const { username,name, password } = req.body;
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = new User({
-        username,
-        name,
-        passwordHash,
-    });
-    const saveduser = await user.save();
-
-    res.status(200).json(saveduser);
-
+    const { username, name, password } = req.body;
+        
+        const passwordHash = await bcrypt.hash(password, 10);
+    
+        const user = new User({
+            username,
+            name,
+            passwordHash,
+        });
+        const saveduser = await user.save();
+    
+        res.status(200).json(saveduser);
 
 });
 
