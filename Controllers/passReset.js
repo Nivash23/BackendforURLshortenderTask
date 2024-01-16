a250449cf256f3672a242a59cf826a2dc274a1ff
Nivash23 <nivashsubash32@gmail.com>
2024-01-16 11:40:09 +0530

    Resetpass feature setting completed..

---
+++ Controllers/passReset.js
@@ -0,0 +1,26 @@
+const PassResetRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../Model/users');
+
+PassResetRouter.post("/", async (req, res) => {
+    console.log(req.body)
+    const { username, newpass, confirmpass } = req.body;
+    const changedpass = await bcrypt.hash(confirmpass, 10);
+    const user = await User.findOne({ username });
+    user.passwordHash = changedpass;
+    await user.save();
+    if (user)
+    {
+        
+        res.status(200).json({
+            message:'password changed sucessfully'
+        })
+    }
+    else {
+        res.status(401).json("Try again...");
+    }
+
+
+    // await user.save();
+})
+module.exports = PassResetRouter;
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const userRouter = require('./Controllers/user');
 const loginRouter = require('./Controllers/loginRouter');
 const EntryRouter = require('./Controllers/entry');
+const PassResetRouter = require('./Controllers/passReset');
 
 app.use(cors());
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/Login', loginRouter);
 app.use('/api/entry', EntryRouter);
+app.use('/api/Resetpass',PassResetRouter);
 
 module.exports = app;
 
