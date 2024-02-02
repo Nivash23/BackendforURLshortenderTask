0848d3daa8c20aea16bf0c10edf2c58246659647
Nivash23 <nivashsubash32@gmail.com>
2024-02-02 17:55:03 +0530

    Added mail send feature

--- Controllers/passReset.js
+++ Controllers/passReset.js
@@ -2,9 +2,11 @@ const PassResetRouter = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../Model/users');
 
+
 PassResetRouter.post("/", async (req, res) => {
     console.log(req.body)
     const { username, newpass, confirmpass } = req.body;
+    
     const changedpass = await bcrypt.hash(confirmpass, 10);
     const user = await User.findOne({ username });
     user.passwordHash = changedpass;
--- Controllers/user.js
+++ Controllers/user.js
@@ -1,6 +1,8 @@
 const userRouter = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../Model/users');
+const nodemailer = require('nodemailer');
+
 
 userRouter.post('/', async (req, res) => {
     const { username, name, password } = req.body;
@@ -12,6 +14,35 @@ userRouter.post('/', async (req, res) => {
             name,
             passwordHash,
         });
+     const html = `
+    <h1>Hello</h1>
+    <p>Welcome to our website,${name}</p>
+    `
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'billanivash52@gmail.com',
+            pass: 'yglubdayulcahfhr'
+        }
+    });
+    const mailOption = {
+        from: 'billanivash52@gmail.com',
+        to: `${username}`,
+        subject: 'MailFromWebsite',
+        html:html,
+    }
+    if (user)
+    {
+        transporter.sendMail(mailOption, function (err, info) {
+            if (err)
+            {
+                console.log(err);
+            }
+            else {
+                console.log(info.response);
+            }
+       })
+        }
         const saveduser = await user.save();
     
         res.status(200).json(saveduser);
